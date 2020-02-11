6619e02b5edf69d9c65205a04645b73207327d79
Whubs333 <Allen.Hsing@gmail.com>
2020-02-11 17:15:42 +0800

    added win conditions

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let board;
 const squares = Array.from(document.querySelectorAll('#board div'))
+const messages = document.querySelector('h2')
 let turn = "X"
+let win;
 document.getElementById('board').addEventListener('click', handleTurn)
+document.getElementById('reset-button').addEventListener('click', start)
+
+start()
 function start(){
     board = [
         "","","",
@@ -11,12 +16,12 @@ function start(){
 marker()
 }
 
-start()
 
 function marker(){
     board.forEach(function(mark, index){
         squares[index].textContent= mark;
     })
+    messages.textContent = win === "T"? `It is a tie!`: win ? `${win} wins!` : `It is ${turn}'s turn`;
 }   
 
 function handleTurn(event){
@@ -25,5 +30,26 @@ function handleTurn(event){
     });
     board[idx] = turn;
     turn = turn == "X" ? "O" : "X";
+    win = getWinner()
     marker()
+}
+
+let winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+function getWinner(){
+    let winner = null
+    winningCombos.forEach(function(combo, index){
+        if(board[combo[0]] && board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]] && board[combo[0]])
+        winner = board[combo[0]];
+    })
+    return winner ? winner : board.includes("") ? null : "T";
 }
