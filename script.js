88d7f1551a99c7a452d65d4a3c52fcf1e0ff2180
Whubs333 <Allen.Hsing@gmail.com>
2020-02-11 16:27:55 +0800

    added marker functionality and turn logic to board

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let board;
 const squares = Array.from(document.querySelectorAll('#board div'))
-
-
+let turn = "X"
+document.getElementById('board').addEventListener('click', handleTurn)
 function start(){
     board = [
         "","","",
@@ -9,14 +9,21 @@ function start(){
         "","",""
     ];
 marker()
-
 }
+
 start()
-
-
 
 function marker(){
     board.forEach(function(mark, index){
         squares[index].textContent= mark;
     })
+}   
+
+function handleTurn(event){
+    let idx = squares.findIndex(function(square){
+        return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn == "X" ? "O" : "X";
+    marker()
 }
